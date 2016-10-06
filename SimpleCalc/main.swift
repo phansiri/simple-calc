07fd5c14dd733cdde7e8750b9c9f8f739a43d621
lit_phansiri <phansiri@uw.edu>
2016-10-06 07:38:21 -0700

    Initial Commit

---
+++ SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Litthideth Phansiri on 10/6/16.
+//  Copyright © 2016 Lit Phansiri. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
